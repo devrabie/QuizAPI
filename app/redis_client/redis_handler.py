280ed8f3d310c952630495df7740f4ccd8be7759
devrabie <rabiaaza1@gmail.com>
2025-08-01 00:32:41 +0300

    feat: Refactor quiz handling to use unique identifiers for quizzes and improve message updates

--- app/redis_client/redis_handler.py
+++ app/redis_client/redis_handler.py
@@ -8,7 +8,7 @@ logger = logging.getLogger(__name__)
 
 REDIS_HOST = os.getenv("REDIS_HOST", "localhost")
 REDIS_PORT = int(os.getenv("REDIS_PORT", 6379))
-REDIS_DB = int(os.getenv("REDIS_DB", 0))
+REDIS_DB = int(0)
 
 redis_client = redis.Redis(host=REDIS_HOST, port=REDIS_PORT, db=REDIS_DB, decode_responses=True)
 
